653fd0fd4a78177fa67ca8027a6ed435e2cc0f8f
ktussey <ktussey@gearheartconstruction.com>
2026-03-11 15:56:16 -0500

    trying to deploy to deploy latest to netlify

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import project from "./sanity/schemas/project";
 
-// Project ID and dataset are set in the .env file
 const projectId = import.meta.env.SANITY_PROJECT_ID || "n3ywke92";
 const dataset = import.meta.env.SANITY_DATASET || "production";
 
