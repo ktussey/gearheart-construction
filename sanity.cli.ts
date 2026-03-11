43ce5f714f4becb0990761dafaedfe20206c5437
TRamos5 <travis.ramos5@gmail.com>
2026-03-11 15:43:12 -0500

    update sanity project to client account

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+	api: {
+		projectId: "n3ywke92",
+		dataset: "production",
+	},
+});
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'sanity';
-import { structureTool } from 'sanity/structure';
-import project from './sanity/schemas/project';
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import project from "./sanity/schemas/project";
 
-const projectId = import.meta.env.SANITY_PROJECT_ID || 'zjs2x4qk';
-const dataset = import.meta.env.SANITY_DATASET || 'production';
+const projectId = import.meta.env.SANITY_PROJECT_ID || "n3ywke92";
+const dataset = import.meta.env.SANITY_DATASET || "production";
 
 export default defineConfig({
-  name: 'gearheart-construction',
-  title: 'Gearheart Construction',
-  projectId,
-  dataset,
-  plugins: [structureTool()],
-  schema: {
-    types: [project],
-  },
+	name: "gearheart-construction",
+	title: "Gearheart Construction",
+	projectId,
+	dataset,
+	plugins: [structureTool()],
+	schema: {
+		types: [project],
+	},
 });
