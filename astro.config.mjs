2906335528623983da14ca9615a8d1523bbf37f7
TRamos5 <travis.ramos5@gmail.com>
2026-03-12 10:49:05 -0500

    added sitemap integraion and placeholders for g4 and search console

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import sanity from '@sanity/astro';
+import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
 
@@ -10,6 +11,7 @@ import netlify from '@astrojs/netlify';
 const env = loadEnv(process.env.NODE_ENV || '', process.cwd(), 'SANITY');
 
 export default defineConfig({
+  site: 'https://gearheartconstruction.com',
   output: 'server',
   adapter: netlify(),
   env: {
@@ -23,6 +25,7 @@ export default defineConfig({
   },
   integrations: [
     react(),
+    sitemap(),
     sanity({
       projectId: env.SANITY_PROJECT_ID,
       dataset: env.SANITY_DATASET || 'production',
